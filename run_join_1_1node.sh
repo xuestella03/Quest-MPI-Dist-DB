26f551d91c07e89f5f2790549ba2fe06849771e0
xuestella03 <xuestella03@gmail.com>
2025-06-03 22:30:42 -0500

    More testing

--- run_join_1_1node.sh
+++ run_join_1_1node.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=distributed_join_customer_orders
 #SBATCH --account=e32695
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4G
 #SBATCH --time=00:20:00
 #SBATCH --partition=short
-#SBATCH --output=join_1_4nodes_%j.out
-#SBATCH --error=join_1_4nodes_%j.err
+#SBATCH --output=join_1_1nodes_%j.out
+#SBATCH --error=join_1_1nodes_%j.err
 
 echo "Starting distributed join for customer and orders tables"
 echo "Job ID: $SLURM_JOB_ID"
@@ -22,13 +22,6 @@ module load mpi/openmpi-4.1.1
 # pip install --user duckdb mpi4py
 
 # echo "Successfully loaded modules and installed packages"
-
-# INPUT_DB="/gpfs/home/exy4679/duckdb_test/data/coordinator/full_data/whole_tpch_0.1.duckdb"
-# LOCAL_DB="/tmp/whole_tpch_${SLURM_JOB_ID}.duckdb"
-# cp "$INPUT_DB" "$LOCAL_DB"
-
-# export DUCKDB_PATH="$LOCAL_DB"
-# echo "Copied DuckDB to $LOCAL_DB"
 
 # Run the tpch file upload test
 # python3 -u src/generate_tpch.py
