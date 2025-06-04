4f774d380b2e3aa3a96cbbb17789db262d9672bc
xuestella03 <xuestella03@gmail.com>
2025-06-04 14:43:08 -0500

    3 nodes

---
+++ run_batch_jobs_seq_1_3nodes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+module load mamba/24.3.0
+module load mpi/openmpi-4.1.1
+
+# Submit the first job
+jobid=$(sbatch run_join_1_3nodes.sh | awk '{print $4}')
+echo "Submitted job 1 with ID $jobid"
+
+# Submit the remaining 99 jobs, each depending on the one before it
+for i in {2..10}; do
+    jobid=$(sbatch --dependency=afterok:$jobid run_join_1_3nodes.sh | awk '{print $4}')
+    echo "Submitted job $i with ID $jobid"
+done
